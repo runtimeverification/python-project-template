b94c940a20c958abc9ff1ab402dbf81db0ee079b
Tamas Toth <tamas.toth@runtimeverification.com>
2022-09-01 10:08:20 +0200

    Fix name of test class

--- {{cookiecutter.project_slug}}/src/tests/test_{{cookiecutter.package_name}}.py
+++ {{cookiecutter.project_slug}}/src/tests/test_{{cookiecutter.package_name}}.py
@@ -3,6 +3,6 @@ from unittest import TestCase
 from {{ cookiecutter.package_name }} import __version__
 
 
-class TestVersion(TestCase):
+class VersionTest(TestCase):
     def test_version(self):
         self.assertEqual(__version__, '{{ cookiecutter.version }}')
