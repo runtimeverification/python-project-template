ed9f5d55297f7db6a5a138a02a79e88e509e870c
Tamas Toth <tamas.toth@runtimeverification.com>
2022-11-22 14:44:44 +0100

    Add pytest

--- {{cookiecutter.project_slug}}/src/tests/test_{{cookiecutter.package_name}}.py
+++ {{cookiecutter.project_slug}}/src/tests/test_{{cookiecutter.package_name}}.py
@@ -1,8 +1,5 @@
-from unittest import TestCase
-
 from {{ cookiecutter.package_name }} import __version__
 
 
-class VersionTest(TestCase):
-    def test_version(self) -> None:
-        self.assertEqual(__version__, '{{ cookiecutter.version }}')
+def test_version() -> None:
+    assert __version__ == '{{ cookiecutter.version }}'
